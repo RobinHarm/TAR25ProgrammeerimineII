48e127149e903c268a7b3e72f42b91f971085d5e
Opilane Techno+TLN <237692821+RobinHarm@users.noreply.github.com>
2026-09-03 14:13:13 +0300

    SpaceshipDto

---
+++ ShopTAR25/ShopTAR25.Core/Dto/SpaceshipDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopTAR25.Core.Dto
+{
+    //Dto class vahendab andmeid rakenduse ja andmebaasi vahel.
+    public class SpaceshipDto
+    {
+    }
+}
