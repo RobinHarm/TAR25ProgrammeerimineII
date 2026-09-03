ce199bc838b5ee259d3aff0a020266616a73cc77
Opilane Techno+TLN <237692821+RobinHarm@users.noreply.github.com>
2026-09-03 13:57:57 +0300

    Spaceship
    Lisasime andmed sisse sinna

--- ShopTAR25/ShopTAR25.Core/Domain/Spaceship.cs
+++ ShopTAR25/ShopTAR25.Core/Domain/Spaceship.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace ShopTAR25.Core.Domain
 {
-    internal class Spaceship
+    public class Spaceship
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Classification { get; set; } = string.Empty;
+        public DateTime? BuildDate { get; set; }
+        public int? Crew { get; set; }
+        public int? EnginePower { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? Modified { get; set; }
+
     }
 }
