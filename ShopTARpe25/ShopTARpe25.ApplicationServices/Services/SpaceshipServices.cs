328f7ef851b2bc0c2040dc10601953666cade69b
KevinKuur <237692821+RobinHarm@users.noreply.github.com>
2026-09-07 14:09:39 +0300

    SpaceShip
    Kosmoselaev

--- ShopTARpe25/ShopTARpe25.ApplicationServices/Services/SpaceshipServices.cs
+++ ShopTARpe25/ShopTARpe25.ApplicationServices/Services/SpaceshipServices.cs
@@ -1,10 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using Microsoft.EntityFrameworkCore;
+using ShopTARpe25.Core.Domain;
+using ShopTARpe25.Core.Dto;
+using ShopTARpe25.Data;
+using System.Xml.Linq;
 
 namespace ShopTARpe25.ApplicationServices.Services
 {
+    
+
     public class SpaceshipServices
     {
+
+        private readonly ShopTARpe25Context _context;
+
+        public SpaceshipServices
+            (
+            ShopTARpe25Context context
+            )
+        {
+            _context = context;
+        }
+        public async Task<Spaceship> Create(SpaceshipDto dto)
+        {
+            Spaceship domain = new();
+
+            domain.Id = dto.Id;
+            domain.Name = dto.Name;
+            domain.Classification = dto.Classification;
+            domain.BuiltDate = dto.BuiltDate;
+            domain.CreatedAt = dto.CreatedAt;
+            domain.Crew = dto.Crew;
+            domain.EnginePower = dto.EnginePower;
+            domain.ModifiedAt = dto.ModifiedAt;
+
+            //siia tuleb kood, mis salvestab domain
+            //objekti andmebaas
+            //tuleb kasutada repository'd mis on 
+            //defineeritud Core projektis
+            //konstruktor kaudu tuleb injectida repository
+
+            await _context.Spaceships.AddAsync(domain);
+            await _context.SaveChangesAsync();
+            
+
+            return domain;
+        }
     }
 }
