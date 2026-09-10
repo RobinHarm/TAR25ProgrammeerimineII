113c9b186b467b65eab4d18aa8ccc5cdb8290fce
Opilane Techno+TLN <237692821+RobinHarm@users.noreply.github.com>
2026-09-10 14:12:12 +0300

    SqlServer ja Service

--- ShopTARpe25/ShopTARpe25/Program.cs
+++ ShopTARpe25/ShopTARpe25/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using ShopTARpe25.ApplicationServices.Services;
+using ShopTARpe25.Core.ServiceInterface;
+using ShopTARpe25.Data;
+
 namespace ShopTARpe25
 {
     public class Program
@@ -5,9 +10,17 @@ namespace ShopTARpe25
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<ISpaceshipServices, SpaceshipServices>();
+            //selleks, et tuleb installida Microsoft.EntityFrameworkCore.SqlServer ja 
+            //Microsoft.EntityFrameworkCore.Tools NuGet paketid
+            //kui installitud, siis viidata napespacesis Microsoft.EntityFrameworkCore-le
+            builder.Services.AddDbContext<ShopTARpe25Context>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
