3e660f9a9b3c6551ce763a5487c3bc0c5e7a50a9
KevinKuur <237692821+RobinHarm@users.noreply.github.com>
2026-09-14 14:00:21 +0300

    migration/databaseconectionstring

--- ShopTARpe25/ShopTARpe25.Data/ShopTARpe25Context.cs
+++ ShopTARpe25/ShopTARpe25.Data/ShopTARpe25Context.cs
@@ -7,6 +7,7 @@ namespace ShopTARpe25.Data
     //teha sellest classist DbContext, et saaks andmebaasi kasutada
     public class ShopTARpe25Context : DbContext
     {
+        public ShopTARpe25Context() { }
         public ShopTARpe25Context(DbContextOptions<ShopTARpe25Context> options)
             : base(options)
         {
